b5515fd2d345e5edc17cdef5f4d7541f50436ef8
AnemonOFF <anemonoff@users.noreply.github.com>
2024-08-15 04:32:23 +0500

    Sidebar present option

--- src/components/sidebar/sidebarElement.tsx
+++ src/components/sidebar/sidebarElement.tsx
@@ -11,6 +11,7 @@ export interface SidebarElementProps {
   year: string;
   url: string;
   highlightClassName?: string;
+  isPresent?: boolean;
 }
 
 const SidebarElement: React.FC<SidebarElementProps> = ({
@@ -18,16 +19,20 @@ const SidebarElement: React.FC<SidebarElementProps> = ({
   year,
   url,
   highlightClassName,
+  isPresent,
 }) => {
   const pathname = usePathname();
   const isActive = pathname == url;
   const className = `bg-gradient-to-b ${highlightClassName}`;
+  const presentClassName = isPresent
+    ? "text-blue-400 font-bold"
+    : "text-foreground-400";
 
   return (
     <Link className={cn("rounded-xl pl-1 w-full", className)} href={url}>
       <div className="rounded-lg p-2 bg-background hover:bg-foreground-100 transition-colors flex gap-5 items-center justify-between w-full">
         <span className="text-lg text-foreground">{name}</span>
-        <span className="text-foreground-400">{year}</span>
+        <span className={presentClassName}>{year}</span>
       </div>
     </Link>
   );
