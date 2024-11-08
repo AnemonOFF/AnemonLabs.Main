732534393e9b753920637f1fa58425c4c5367015
AnemonOFF <anemonoff@users.noreply.github.com>
2024-11-09 00:19:57 +0500

    fix typography dark mode in mdx

--- src/app/[lang]/project/[name]/page.tsx
+++ src/app/[lang]/project/[name]/page.tsx
@@ -74,7 +74,7 @@ export default async function ProjectPage({
               </div>
             ))}
           </div>
-          <div className="mt-5 prose prose-headings:mt-5 prose-headings:font-semibold prose-h1:text-5xl prose-h2:text-4xl prose-h3:text-3xl prose-h4:text-2xl prose-h5:text-xl prose-h6:text-lg">
+          <div className="mt-5 prose prose-headings:mt-5 prose-headings:font-semibold prose-h1:text-5xl prose-h2:text-4xl prose-h3:text-3xl prose-h4:text-2xl prose-h5:text-xl prose-h6:text-lg dark:prose-invert prose-zinc">
             <MDXRemote source={markdown} />
           </div>
         </div>
